10e8b0d291779948387dfc633531639535737d0d
ibrahimmalik <ibrahimmohammadmalik@icloud.com>
2023-09-12 18:45:39 -0400

    Finished program that reduces from 100 to 0

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+
+        //Number n
+        int i = 100;
+
+        //number of steps
+        int steps = 0;
+
+        //While loop to make sure current number is above 0
+        while (i > 0){
+
+            steps++;
+
+            //Calculations
+            boolean evenOdd = i % 2 == 0;
+
+            //If statement to check even or odd
+            if (evenOdd){
+                i = i / 2;
+            }
+            else {
+                i -= 1;
+            }
+        }
+
+        System.out.println("With the current algorithm, it takes " + steps + " steps to reach 0.");
+    }
 }
